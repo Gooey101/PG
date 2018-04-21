a4370b10157d5cf9c68f685c31d2cab2a05dd2ae
christophergu <clg2n4@mst.edu>
2018-04-21 12:41:42 -0500

    added account info to a shared preference file called "accountInfo"

--- app/src/main/java/com/example/christophergu/pg/MainActivity.java
+++ app/src/main/java/com/example/christophergu/pg/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.christophergu.pg;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -20,12 +22,28 @@ public class MainActivity extends AppCompatActivity {
         mDob = findViewById(R.id.dob);
 
         Intent intent = getIntent();
-        String phone = intent.getStringExtra(SignInActivity.strPhone);
-        String username = intent.getStringExtra(SignInActivity.strUsername);
-        String dob = intent.getStringExtra(SignInActivity.strDob);
-        Account account = new Account(phone, username, dob);
+        String intentPhone = intent.getStringExtra(SignInActivity.strPhone);
+        String intentUsername = intent.getStringExtra(SignInActivity.strUsername);
+        String intentDob = intent.getStringExtra(SignInActivity.strDob);
 
-        mUsername.setText(username);
-        mDob.setText(dob);
+        // Set account information
+        mUsername.setText(intentUsername);
+        mDob.setText(intentDob);
+
+        // Write to shared preferences
+        SharedPreferences writeSharedPref = this.getSharedPreferences("accountInfo", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = writeSharedPref.edit();
+        editor.putString("accountPhone", intentPhone);
+        editor.putString("accountUsername", intentUsername);
+        editor.putString("accountDob", intentDob);
+        editor.apply();
+
+        // Read from shared preferences
+        /*
+        SharedPreferences readSharedPref = getSharedPreferences("accountInfo", Context.MODE_PRIVATE);
+        String phone = readSharedPref.getString("accountPhone", null);
+        String username = readSharedPref.getString("accountUsername", null);
+        String dob = readSharedPref.getString("accountDob", null);
+        */
     }
 }
